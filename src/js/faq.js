812ca0454c9d19da0b206e68be2ca4cc4aa649b1
hiper <ivan.yuschuk77@gmail.com>
2025-12-16 00:46:58 +0200

    added chenges

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,11 +5,11 @@ new Accordion('.accordion-container', {
     duration: 400,
     showMultiple: false,
     onOpen: (currentElement) => {
-        const icon = currentElement.querySelector("use");
-        icon.href.baseVal = "../img/sprite.svg#icon-close";
+        const icon = currentElement.querySelector(".faq-icon-use");
+        icon.href.baseVal = "/img/sprite.svg#icon-close";
     },
     onClose: (currentElement) => {
-        const icon = currentElement.querySelector("use");
-        icon.href.baseVal = "../img/sprite.svg#icon-add";
+        const icon = currentElement.querySelector(".faq-icon-use");
+        icon.href.baseVal = "/img/sprite.svg#icon-add";
     }
 }); 
