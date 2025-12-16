d419b9932610d8b5fb453ad7be9d0073ef0dad78
Skyzary <132926053+Skyzary@users.noreply.github.com>
2025-12-16 13:54:47 +0100

    Add mobile menu implementation with JavaScript, HTML, and CSS. Remove unused navigation links from the header.

--- src/js/mobile-menu.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.querySelector('.location');
-const closeBtn = document.querySelector('.close-btn');
-closeBtn.addEventListener('click', () => {
-  mobileMenu.classList.remove('is-visible');
-});
-
